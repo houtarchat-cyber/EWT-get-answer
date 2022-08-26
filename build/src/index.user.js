7bdc90822199788c0491bd1985c5013e0f036871
Houtarchat <admin@houtarchat.ml>
2022-08-26 23:01:21 +0800

    init commit

---
+++ build/src/index.user.js
@@ -0,0 +1,88 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// ==UserScript==
+// @name           升学 E 网通 (EWT360) 试题答案获取 - Beta
+// @name:en        EWT Exam Answers Getter - Beta
+// @namespace      https://ewt.houtar.eu.org/examanswer2
+// @version        0.1.1
+// @description    此脚本在 EWT 试题中获取试题答案。
+// @description:en This script gets exam answers in EWT exam.
+// @author         Houtar
+// @match          https://web.ewt360.com/mystudy/
+// @icon           https://web.ewt360.com/favicon.ico
+// @grant          none
+// @license        GNU General Public License
+// ==/UserScript==
+(() => {
+    'use strict';
+    if (!document.location.hash.includes('exam/answer')) {
+        return;
+    }
+    const getAnswersByPaperData = (d) => __awaiter(void 0, void 0, void 0, function* () {
+        const getAnswerByQuestionId = (questionId, isChildQuestion) => __awaiter(void 0, void 0, void 0, function* () {
+            const res = yield fetch(`https://web.ewt360.com/customerApi/api/studyprod/web/answer/quesiton/analysis?questionId=${questionId}&` +
+                document.location.hash.slice(14));
+            const resJson = yield res.json();
+            if (isChildQuestion === true) {
+                return resJson.data.childQuestions.map((element) => element.rightAnswer.join());
+            }
+            return resJson.data.rightAnswer.join();
+        });
+        let answers = '';
+        for (let key = 0; key < d.data.questions.length; key++) {
+            const element = d.data.questions[key];
+            const questionNum = key + 1;
+            const childQuestions = element.childQuestions;
+            if (childQuestions.length === 0) {
+                let answer = yield getAnswerByQuestionId(element.id);
+                answer = answer === '' ? element.analyse : answer;
+                answers += `<h4>${questionNum}: ${answer}</h4>`;
+            }
+            else {
+                const childQuestionAnswers = yield getAnswerByQuestionId(element.id, true);
+                for (let childQuestionKey = 0; childQuestionKey < childQuestions.length; childQuestionKey++) {
+                    const childQuestion = childQuestions[childQuestionKey];
+                    const childQuestionNum = childQuestionKey + 1;
+                    const childQuestionAnswer = childQuestionAnswers[childQuestionKey] === ''
+                        ? childQuestion.analyse
+                        : childQuestionAnswers[childQuestionKey];
+                    answers +=
+                        `<h4>${questionNum}. (${childQuestionNum})` +
+                            ` : ${childQuestionAnswer}</h4>`;
+                }
+            }
+        }
+        return answers;
+    });
+    fetch('https://web.ewt360.com/customerApi/api/studyprod/web/answer/paper' +
+        document.location.hash.slice(13))
+        .then((p) => __awaiter(void 0, void 0, void 0, function* () { return yield p.json(); }))
+        .then((d) => {
+        getAnswersByPaperData(d)
+            .then((answers) => {
+            const answerShower = window.open('', '_blank', 'popup');
+            if (answerShower !== null) {
+                answerShower.document.body.innerHTML = answers;
+            }
+            else {
+                window.alert('答案窗口未弹出。\n这可能是由于您的浏览器阻止了弹出窗口。');
+            }
+        })
+            .catch((e) => {
+            console.error(e);
+        });
+    })
+        .catch((e) => {
+        console.error(e);
+    });
+})();
+//# sourceMappingURL=index.user.js.map
---
+++ src/index.user.ts
@@ -0,0 +1,96 @@
+import {AnalysisData} from './AnalysisData';
+import {PaperData, Question} from './PaperData';
+
+// ==UserScript==
+// @name           升学 E 网通 (EWT360) 试题答案获取 - Beta
+// @name:en        EWT Exam Answers Getter - Beta
+// @namespace      https://ewt.houtar.eu.org/examanswer2
+// @version        0.1.1
+// @description    此脚本在 EWT 试题中获取试题答案。
+// @description:en This script gets exam answers in EWT exam.
+// @author         Houtar
+// @match          https://web.ewt360.com/mystudy/
+// @icon           https://web.ewt360.com/favicon.ico
+// @grant          none
+// @license        GNU General Public License
+// ==/UserScript==
+(() => {
+  'use strict';
+  if (!document.location.hash.includes('exam/answer')) {
+    return;
+  }
+  const getAnswersByPaperData = async (d: PaperData) => {
+    const getAnswerByQuestionId = async (
+      questionId: string,
+      isChildQuestion?: Boolean
+    ): Promise<string | string[]> => {
+      const res = await fetch(
+        `https://web.ewt360.com/customerApi/api/studyprod/web/answer/quesiton/analysis?questionId=${questionId}&` +
+          document.location.hash.slice(14)
+      );
+      const resJson: AnalysisData = await res.json();
+      if (isChildQuestion === true) {
+        return (resJson.data.childQuestions as Question[]).map(
+          (element): string => element.rightAnswer.join()
+        );
+      }
+      return resJson.data.rightAnswer.join();
+    };
+    let answers = '';
+    for (let key = 0; key < d.data.questions.length; key++) {
+      const element = d.data.questions[key];
+      const questionNum = key + 1;
+      const childQuestions = element.childQuestions;
+      if (childQuestions.length === 0) {
+        let answer = await getAnswerByQuestionId(element.id);
+        answer = answer === '' ? element.analyse : answer;
+        answers += `<h4>${questionNum}: ${answer}</h4>`;
+      } else {
+        const childQuestionAnswers = await getAnswerByQuestionId(
+          element.id,
+          true
+        );
+        for (
+          let childQuestionKey = 0;
+          childQuestionKey < childQuestions.length;
+          childQuestionKey++
+        ) {
+          const childQuestion = childQuestions[childQuestionKey];
+          const childQuestionNum = childQuestionKey + 1;
+          const childQuestionAnswer =
+            childQuestionAnswers[childQuestionKey] === ''
+              ? childQuestion.analyse
+              : childQuestionAnswers[childQuestionKey];
+          answers +=
+            `<h4>${questionNum}. (${childQuestionNum})` +
+            ` : ${childQuestionAnswer}</h4>`;
+        }
+      }
+    }
+    return answers;
+  };
+  fetch(
+    'https://web.ewt360.com/customerApi/api/studyprod/web/answer/paper' +
+      document.location.hash.slice(13)
+  )
+    .then(async (p): Promise<PaperData> => await p.json())
+    .then((d: PaperData) => {
+      getAnswersByPaperData(d)
+        .then((answers): void => {
+          const answerShower = window.open('', '_blank', 'popup');
+          if (answerShower !== null) {
+            answerShower.document.body.innerHTML = answers;
+          } else {
+            window.alert(
+              '答案窗口未弹出。\n这可能是由于您的浏览器阻止了弹出窗口。'
+            );
+          }
+        })
+        .catch((e): void => {
+          console.error(e);
+        });
+    })
+    .catch((e): void => {
+      console.error(e);
+    });
+})();
